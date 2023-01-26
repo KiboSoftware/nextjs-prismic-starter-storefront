d6f17919be41a1b10556e9f701143ba3bdb15b62
kibo-chandradeeptalaha <89371824+kibo-chandradeeptalaha@users.noreply.github.com>
2023-01-26 12:32:34 +0530

    test commit

--- lib/api/handlers/search.ts
+++ lib/api/handlers/search.ts
@@ -6,8 +6,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default async function searchHandler(req: NextApiRequest, res: NextApiResponse) {
   try {
     // get variables
-    const response = await productSearch(req.query as unknown as CategorySearchParams)
-    res.status(200).json({ results: response?.data?.products })
+    // const response = await productSearch(req.query as unknown as CategorySearchParams)
+    res.status(200).json({ results: 'ok' })
   } catch (error) {
     console.error(error)
     const message = 'An unexpected error ocurred'
